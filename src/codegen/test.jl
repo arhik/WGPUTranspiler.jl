07cc533e3a1634f97f7f8202e4d83a0ee4342831
Arhik <arhik23@gmail.com>
2024-03-10 21:07:18 +0530

    transpile ifblock basic working

--- src/codegen/test.jl
+++ src/codegen/test.jl
@@ -35,3 +35,14 @@ inferredExpr = inferExpr(
 	end)
 )
 transpile(scope, inferredExpr)
+
+scope = Scope([:a, :d, :b, :c], [:g, :i, :println, :+], 0, nothing, quote end)
+inferredExpr = inferExpr(
+	scope, 
+	:(if a > 0
+		println(i) 
+		a[i] = b[i]
+		c[i] = d[i] + c[i] + 1.0
+	end)
+)
+transpile(scope, inferredExpr)
--- src/codegen/transpile.jl
+++ src/codegen/transpile.jl
@@ -1,27 +1,27 @@
 export transpile 
 
-transpile(scope, s::Scalar) = s.element
-transpile(scope, var::WGPUVariable) = var.dataType == Any ? :($(var.sym)) : :($(var.sym)::$(var.dataType))
-transpile(scope, lhs::LHS) = transpile(scope, lhs.variable, Val(lhs.mutable))
-transpile(scope, var::WGPUVariable, ::Val{false}) = :(@var $(transpile(scope, var)))
-transpile(scope, var::WGPUVariable, ::Val{true}) = :($(var.sym))
-transpile(scope, rhs::RHS) = transpile(scope, rhs.rhsExpr)
-transpile(scope, binOp::BinaryOp) = transpile(scope, binOp, Val(binOp.op))
+transpile(scope::Scope, s::Scalar) = s.element
+transpile(scope::Scope, var::WGPUVariable) = var.dataType == Any ? :($(var.sym)) : :($(var.sym)::$(var.dataType))
+transpile(scope::Scope, lhs::LHS) = transpile(scope, lhs.variable, Val(lhs.mutable))
+transpile(scope::Scope, var::WGPUVariable, ::Val{false}) = :(@var $(transpile(scope, var)))
+transpile(scope::Scope, var::WGPUVariable, ::Val{true}) = :($(var.sym))
+transpile(scope::Scope, rhs::RHS) = transpile(scope, rhs.rhsExpr)
+transpile(scope::Scope, binOp::BinaryOp) = transpile(scope, binOp, Val(binOp.op))
 
 # for each op in [:+, :-, :*, :\, :<, :>, :<=, :>=, :==, :+=, :-=]
-transpile(scope, binOp::BinaryOp, op::Val{:+}) = :($(transpile(scope, binOp.left)) + $(transpile(scope, binOp.right)))
-transpile(scope, binOp::BinaryOp, op::Val{:-}) = :($(transpile(scope, binOp.left)) - $(transpile(scope, binOp.right)))
-transpile(scope, binOp::BinaryOp, op::Val{:*}) = :($(transpile(scope, binOp.left)) * $(transpile(scope, binOp.right)))
-transpile(scope, binOp::BinaryOp, op::Val{:/}) = :($(transpile(scope, binOp.left)) / $(transpile(scope, binOp.right)))
-transpile(scope, binOp::BinaryOp, op::Val{:<}) = :($(transpile(scope, binOp.left)) < $(transpile(scope, binOp.right)))
-transpile(scope, binOp::BinaryOp, op::Val{:>}) = :($(transpile(scope, binOp.left)) > $(transpile(scope, binOp.right)))
-transpile(scope, binOp::BinaryOp, op::Val{:(<=)}) = :($(transpile(scope, binOp.left)) <= $(transpile(scope, binOp.right)))
-transpile(scope, binOp::BinaryOp, op::Val{:(>=)}) = :($(transpile(scope, binOp.left)) >= $(transpile(scope, binOp.right)))
-transpile(scope, binOp::BinaryOp, op::Val{:(==)}) = :($(transpile(scope, binOp.left)) == $(transpile(scope, binOp.right)))
-transpile(scope, binOp::BinaryOp, op::Val{:(+=)}) = :($(transpile(scope, binOp.left)) += $(transpile(scope, binOp.right)))
-transpile(scope, binOp::BinaryOp, op::Val{:(-=)}) = :($(transpile(scope, binOp.left)) -= $(transpile(scope, binOp.right)))
+transpile(scope::Scope, binOp::BinaryOp, op::Val{:+}) = :($(transpile(scope, binOp.left)) + $(transpile(scope, binOp.right)))
+transpile(scope::Scope, binOp::BinaryOp, op::Val{:-}) = :($(transpile(scope, binOp.left)) - $(transpile(scope, binOp.right)))
+transpile(scope::Scope, binOp::BinaryOp, op::Val{:*}) = :($(transpile(scope, binOp.left)) * $(transpile(scope, binOp.right)))
+transpile(scope::Scope, binOp::BinaryOp, op::Val{:/}) = :($(transpile(scope, binOp.left)) / $(transpile(scope, binOp.right)))
+transpile(scope::Scope, binOp::BinaryOp, op::Val{:<}) = :($(transpile(scope, binOp.left)) < $(transpile(scope, binOp.right)))
+transpile(scope::Scope, binOp::BinaryOp, op::Val{:>}) = :($(transpile(scope, binOp.left)) > $(transpile(scope, binOp.right)))
+transpile(scope::Scope, binOp::BinaryOp, op::Val{:(<=)}) = :($(transpile(scope, binOp.left)) <= $(transpile(scope, binOp.right)))
+transpile(scope::Scope, binOp::BinaryOp, op::Val{:(>=)}) = :($(transpile(scope, binOp.left)) >= $(transpile(scope, binOp.right)))
+transpile(scope::Scope, binOp::BinaryOp, op::Val{:(==)}) = :($(transpile(scope, binOp.left)) == $(transpile(scope, binOp.right)))
+transpile(scope::Scope, binOp::BinaryOp, op::Val{:(+=)}) = :($(transpile(scope, binOp.left)) += $(transpile(scope, binOp.right)))
+transpile(scope::Scope, binOp::BinaryOp, op::Val{:(-=)}) = :($(transpile(scope, binOp.left)) -= $(transpile(scope, binOp.right)))
 
-function transpile(scope, a::AssignmentExpr)
+function transpile(scope::Scope, a::AssignmentExpr)
 	lExpr = transpile(scope, a.lhs)
 	rExpr = transpile(scope, a.rhs)
 	if @capture(lExpr, @var_(v_))
@@ -31,26 +31,33 @@ function transpile(scope, a::AssignmentExpr)
 	end
 end
 
-function transpile(scope, cExpr::CallExpr)
+function transpile(scope::Scope, cExpr::CallExpr)
 	return Expr(:call, transpile(scope, cExpr.func), map(x -> transpile(scope, x), cExpr.args)...)
 end
 
-function transpile(scope, idxExpr::IndexExpr)
+function transpile(scope::Scope, idxExpr::IndexExpr)
 	return Expr(:ref, transpile(scope, idxExpr.sym), transpile(scope, idxExpr.idx))
 end
 
-transpile(scope, idxExpr::IndexExpr, ::Val{true}) = transpile(scope, idxExpr::IndexExpr)
-transpile(scope, idxExpr::IndexExpr, ::Val{false}) = error("This variable cannot be indexed")
+transpile(scope::Scope, idxExpr::IndexExpr, ::Val{true}) = transpile(scope, idxExpr::IndexExpr)
+transpile(scope::Scope, idxExpr::IndexExpr, ::Val{false}) = error("This variable cannot be indexed")
 
-function transpile(scope, acsExpr::AccessExpr)
+function transpile(scope::Scope, acsExpr::AccessExpr)
 	return Expr(:., transpile(scope, acsExpr.sym), QuoteNode(transpile(scope, acsExpr.field)))
 end
 
-function transpile(scope, rblock::RangeBlock)
+function transpile(scope::Scope, rblock::RangeBlock)
 	(start, step, stop) = map(x -> transpile(scope, x), (rblock.start, rblock.step, rblock.stop))
 	range = :($start:$step:$stop)
 	block = map(x -> transpile(scope, x), rblock.block)
 	idx = transpile(scope, rblock.idx)
 	return Expr(:for, Expr(:(=), idx, range), quote $(block...) end)
+end
+
+function transpile(scope::Scope, ifblock::IfBlock)
+	@infiltrate
+	c = transpile(scope, ifblock.cond)
+	block = map(x -> transpile(scope, x), ifblock.block)
+	return Expr(:if, c, quote $(block...) end)
 end
 
